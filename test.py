c3c95f26fa4e8e408b033f0d44ef03f9c124c524
Sherwood <54739986+Cwaty@users.noreply.github.com>
2021-10-17 22:08:17 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+number_one = int(input("Faktöriyelini bulmak istediğiniz sayıyı girin."))
+
+if number_one <0:
+    print("Faktöriyelini bulmak istediğiniz sayı 0 dan küçük olamaz.")
+else:
+
+    one = 1
+    for i in range(number_one):
+        one = one * (i+1)
+
+    print(range(number_one))
+    print(number_one,"!")
+    print(number_one,"Sayısının faktöriyeli",one)
+    print("Sherwood: https://github.com/Cwaty" , "Crypto: https://github.com/xrypt0")
